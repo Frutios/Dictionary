f390ffc84c5df9e29aba96a8273a7f8c7b7d0288
Frutios <benjamin.tourneur7@gmail.com>
2021-02-28 16:34:33 +0100

    quest resolved

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char,List<string>> dictionary = new Dictionary<char, List<string>>();
+            dictionary['a'] = new List<string> {"Ape", "Awful", "Amazing"};
+            dictionary['b'] = new List<string> {"Beautiful", "Bee", "Butcher"};
+            dictionary['c'] = new List<string> {"Conditions", "Carrot", "Cool"};
+
+            dictionary.Remove('c');
+            int dictionarySize = dictionary.Count;
+            Console.WriteLine(dictionarySize); 
+        }
+    }
+}
